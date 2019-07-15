e94b9d94d8a782b9b64c0332966f85905c14d351
jeffhappily <goodboy96@live.com.my>
2019-07-15 22:41:26 +0800

    Add handle guess

--- hangman/src/Main.hs
+++ hangman/src/Main.hs
@@ -70,6 +70,24 @@ fillInCharacter (Puzzle word filledInSoFar s) c = Puzzle word
 
   newFilledInSoFar = zipWith (zipper c) word filledInSoFar
 
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+  putStrLn $ "Your guess was: " ++ [guess]
+  case (charInWord puzzle guess, alreadyGuessed puzzle guess) of
+    (_, True) -> do
+      putStrLn "You already guessed that character, pick something else!"
+
+      return puzzle
+    (True, _) -> do
+      putStrLn "This character was in the word, filling in the word accordingly"
+
+      return (fillInCharacter puzzle guess)
+    (False, _) -> do
+      putStrLn "This character wasn't in the word, try again"
+      -- Example says `(fillInCharacter puzzle guess)`
+      return puzzle
+
+
 
 main :: IO ()
 main = do

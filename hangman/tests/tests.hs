f4b7afd6848c2b8395eb316e107a51201a30f307
jeffhappily <goodboy96@live.com.my>
2019-07-21 13:10:02 +0800

    Finish tests

--- hangman/tests/tests.hs
+++ hangman/tests/tests.hs
@@ -4,6 +4,7 @@ import           Data.Char
 import           Data.Maybe      (isJust)
 import qualified Data.Set        as Set
 import           Hangman
+import           Test.Hspec
 import           Test.QuickCheck
 
 hasDuplicates :: (Ord a) => [a] -> Bool
@@ -35,6 +36,49 @@ prop_fillInCharacter =
         (True, True) -> cmpDiscoveredLength == EQ && cmpGuessedLength == GT
         (_, _)       -> cmpDiscoveredLength == GT && cmpGuessedLength == GT))
 
+handleGuessSpec :: IO ()
+handleGuessSpec = hspec $ do
+  describe "handleGuess" $ do
+    context "Guessed character is in word" $ do
+      it "guessed length increases" $ do
+        let Puzzle _ newDiscovered newGuessed = fillInCharacter p 'e'
+
+        compare (length newGuessed) (length guessed) `shouldBe` GT
+
+      it "discovered length increases if char is not guessed" $ do
+        let Puzzle _ newDiscovered newGuessed = fillInCharacter p 'e'
+
+        compare
+          (length $ filter isJust newDiscovered)
+          (length $ filter isJust discovered)
+          `shouldBe` GT
+
+      it "discovered length remains if char is guessed" $ do
+        let Puzzle _ newDiscovered newGuessed = fillInCharacter p 'h'
+
+        compare
+          (length $ filter isJust newDiscovered)
+          (length $ filter isJust discovered)
+          `shouldBe` EQ
+
+    context "Char not in word" $ do
+      it "guessed length increases" $ do
+        let Puzzle _ newDiscovered newGuessed = fillInCharacter p 'x'
+
+        compare (length newGuessed) (length guessed) `shouldBe` GT
+
+      it "discovered length remains" $ do
+        let Puzzle _ newDiscovered newGuessed = fillInCharacter p 'x'
+
+        compare
+          (length $ filter isJust newDiscovered)
+          (length $ filter isJust discovered)
+          `shouldBe` EQ
+
+  where
+    p@(Puzzle _ discovered guessed) = Puzzle "hello" [Just 'h', Nothing, Nothing, Nothing, Nothing] "zckih"
+
 main :: IO ()
 main = do
   quickCheck prop_fillInCharacter
+  handleGuessSpec

b52282bbb732207d12e5de77afcb4b076c60c4aa
jeffhappily <goodboy96@live.com.my>
2019-07-23 21:24:38 +0800

    Add mad libs

--- chap15.hs
+++ chap15.hs
@@ -15,4 +15,30 @@ instance Semigroup a => Semigroup (Optional a) where
 instance Monoid a => Monoid (Optional a) where
   mempty = Nada
 
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
 
+madlibbin' :: Exclamation
+  -> Adverb
+  -> Noun
+  -> Adjective
+  -> String
+madlibbin' e adv noun adj =
+  e <> "! he said " <>
+  adv <> " as he jumped into his car " <>
+  noun <> " and drove off with his " <>
+  adj <> " wife."
+
+madlibbinBetter' :: Exclamation
+  -> Adverb
+  -> Noun
+  -> Adjective
+  -> String
+madlibbinBetter' e adv noun adj = mconcat
+  [ e, "! he said "
+  , adv , " as he jumped into his car "
+  , noun , " and drove off with his "
+  , adj , " wife."
+  ]

01d05b48cc1a00aa9b2325560bc48c68e4fe8df7
jeffhappily <goodboy96@live.com.my>
2019-07-31 07:57:33 +0800

    Add RankNTypes extension

--- language_ext.hs
+++ language_ext.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE RankNTypes                #-}
 
-module Test where
+module LanguageExt where
 
 a = 1
 
 sayHello :: String -> IO ()
 sayHello x = putStrLn ("Hello, " ++ x ++ "!")
+
+type Nat f g = forall a . f a -> g a

299892a2461e14134cd05bd87f4f8328e66e0ce1
jeffhappily <goodboy96@live.com.my>
2019-07-31 08:21:36 +0800

    Add some monoid examples

---
+++ monoid.hs
@@ -0,0 +1,11 @@
+module Monoid where
+
+import           Data.Monoid
+
+main :: IO ()
+main = do
+  -- instance Monoid a => Applicative ((,) a)
+  -- first type argument to two-tuple has to be a monoid
+  print $ ("Woo", (+1)) <*> (" Hoo!", 0) -- ("Woo Hoo!",1)
+
+  print $ (Sum 2, (+1)) <*> (Sum 9, 0) -- (Sum {getSum = 11},1)

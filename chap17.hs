270b26d300b30d2dda5861f613e1ae7475a55893
jeffhappily <goodboy96@live.com.my>
2019-08-03 18:01:41 +0800

    Add more types and their instances of Applicative

--- chap17.hs
+++ chap17.hs
@@ -63,6 +63,8 @@ instance Eq a => EqProp (Identity a) where
 instance Arbitrary a => Arbitrary (Identity a) where
   arbitrary = Identity <$> arbitrary
 
+---------------------
+
 newtype Constant a b =
   Constant { getConstant :: a }
   deriving (Eq, Ord, Show)
@@ -80,6 +82,8 @@ instance Eq a => EqProp (Constant a b) where
 
 instance Arbitrary a => Arbitrary (Constant a b) where
   arbitrary = Constant <$> arbitrary
+
+---------------------
 
 newtype ZipList' a =
   ZipList' [a]
@@ -101,6 +105,8 @@ instance Applicative ZipList' where
 
 instance Arbitrary a => Arbitrary (ZipList' a) where
   arbitrary = ZipList' <$> arbitrary
+
+---------------------
 
 data Validation e a
   = Failure e
@@ -127,6 +133,116 @@ instance (Arbitrary e, Arbitrary a) => Arbitrary (Validation e a) where
 instance (Eq e, Eq a) => EqProp (Validation e a) where
   (=-=) = eq
 
+---------------------
+
+data Pair a = Pair a a deriving (Eq, Show)
+
+instance Functor Pair where
+  fmap f (Pair x y) = Pair (f x) (f y)
+
+instance Applicative Pair where
+  pure x = Pair x x
+
+  Pair f g <*> Pair x y = Pair (f x) (g y)
+
+instance Arbitrary a => Arbitrary (Pair a) where
+  arbitrary = Pair <$> arbitrary <*> arbitrary
+
+instance Eq a => EqProp (Pair a) where
+  (=-=) = eq
+
+---------------------
+
+data Two a b = Two a b deriving (Eq, Show)
+
+instance Functor (Two a) where
+  fmap f (Two a b) = Two a $ f b
+
+instance Monoid a => Applicative (Two a) where
+  pure = Two mempty
+
+  (Two a f) <*> (Two a' b') = Two (a <> a') (f b')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = liftA2 Two arbitrary arbitrary
+
+instance (Eq a, Eq b) => EqProp (Two a b) where
+  (=-=) = eq
+
+---------------------
+
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b $ f c
+
+instance (Monoid a, Monoid b) => Applicative (Three a b) where
+  pure = Three mempty mempty
+
+  (Three a b f) <*> (Three a' b' c') = Three (a <> a') (b <> b') (f c')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = liftA3 Three arbitrary arbitrary arbitrary
+
+instance (Eq a, Eq b, Eq c) => EqProp (Three a b c) where
+  (=-=) = eq
+
+---------------------
+
+data Three' a b = Three' a b b deriving (Eq, Show)
+
+instance Functor (Three' a) where
+  fmap f (Three' a b c) = Three' a (f b) (f c)
+
+instance Monoid a => Applicative (Three' a) where
+  pure x = Three' mempty x x
+
+  (Three' a f g) <*> (Three' a' b' c') = Three' (a <> a') (f b') (g c')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+  arbitrary = liftA3 Three' arbitrary arbitrary arbitrary
+
+instance (Eq a, Eq b) => EqProp (Three' a b) where
+  (=-=) = eq
+
+---------------------
+
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance Functor (Four a b c) where
+  fmap f (Four a b c d) = Four a b c $ f d
+
+instance (Monoid a, Monoid b, Monoid c) => Applicative (Four a b c) where
+  pure = Four mempty mempty mempty
+
+  (Four a b c f) <*> (Four a' b' c' d') =
+    Four (a <> a') (b <> b') (c <> c') (f d')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four a b c d) where
+  arbitrary = Four <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+instance (Eq a, Eq b, Eq c, Eq d) => EqProp (Four a b c d) where
+  (=-=) = eq
+
+---------------------
+
+data Four' a b = Four' a a a b deriving (Eq, Show)
+
+instance Functor (Four' a) where
+  fmap f (Four' a b c d) = Four' a b c $ f d
+
+instance Monoid a => Applicative (Four' a) where
+  pure = Four' mempty mempty mempty
+
+  (Four' a b c f) <*> (Four' a' b' c' d') = Four' (a <> a') (b <> b') (c <> c') (f d')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Four' a b) where
+  arbitrary = Four' <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+instance (Eq a, Eq b) => EqProp (Four' a b) where
+  (=-=) = eq
+
+
 main :: IO ()
 main = do
   quickBatch (applicative $ Identity ("hi", 1 :: Int, [1, 2] :: [Int]))
@@ -135,9 +251,31 @@ main = do
   quickBatch
     (applicative $ (Constant "hi" :: Constant String (String, String, String)))
 
-  quickBatch (applicative $ (ZipList' [("hi", 1 :: Int, [1, 2] :: [Int])]))
+  -- It runs too slow...
+  -- quickBatch (applicative $ (ZipList' [("hi", 1 :: Int, [1, 2] :: [Int])]))
 
   quickBatch
     ( applicative
     $ (Success ("hi", 1, [1, 2]) :: Validation String (String, Int, [Int]))
     )
+
+  quickBatch
+    (applicative $ Pair ("hi", 1 :: Int, [1, 2] :: [Int])
+                        ("hi", 1 :: Int, [1, 2] :: [Int])
+    )
+
+  quickBatch (applicative $ Two "ha" ("hi", 1 :: Int, [1, 2] :: [Int]))
+
+  quickBatch (applicative $ Three "ha" "he" ("hi", 1 :: Int, [1, 2] :: [Int]))
+
+  quickBatch
+    (applicative $ Three' "ha"
+                          ("hi", 1 :: Int, [1, 2] :: [Int])
+                          ("hi", 1 :: Int, [1, 2] :: [Int])
+    )
+
+  quickBatch
+    (applicative $ Four "ha" "he" "hu" ("hi", 1 :: Int, [1, 2] :: [Int]))
+
+  quickBatch
+    (applicative $ Four' "ha" "he" "hu" ("hi", 1 :: Int, [1, 2] :: [Int]))

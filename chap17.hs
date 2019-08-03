483917358ab06d306819d181f53dba2dfd5e2089
jeffhappily <goodboy96@live.com.my>
2019-08-03 12:07:43 +0800

    Add applicative instance for identity and constant

--- chap17.hs
+++ chap17.hs
@@ -42,3 +42,26 @@ y'' = lookup 2 $ zip xs'' ys''
 
 summed :: Maybe Integer
 summed = sum <$> liftA2 (,) x'' y''
+
+newtype Identity a = Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity $ f x
+
+instance Applicative Identity where
+  pure = Identity
+
+  Identity f <*> Identity x = Identity $ f x
+
+newtype Constant a b =
+  Constant { getConstant :: a }
+  deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant x) = Constant x
+
+instance Monoid a => Applicative (Constant a) where
+  pure _ = Constant mempty
+
+  Constant x <*> Constant y = Constant $ mappend x y

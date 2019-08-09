746675051b6117704f2da0acfb1a9657109216c6
jeffhappily <goodboy96@live.com.my>
2019-08-10 07:04:44 +0800

    More exercise

--- chap18.hs
+++ chap18.hs
@@ -32,6 +32,57 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Sum a b) where
 instance (Eq a, Eq b) => EqProp (Sum a b) where
   (=-=) = eq
 
+--------------
+
+data Nope a =
+  NopeDotJpg
+  deriving (Eq, Show)
+
+instance Functor Nope where
+  fmap _ NopeDotJpg = NopeDotJpg
+
+instance Applicative Nope where
+  pure _ = NopeDotJpg
+
+  _ <*> _ = NopeDotJpg
+
+instance Monad Nope where
+  NopeDotJpg >>= _ = NopeDotJpg
+
+instance Arbitrary a => Arbitrary (Nope a) where
+  arbitrary = return NopeDotJpg
+
+instance EqProp (Nope a) where
+  (=-=) = eq
+
+--------------
+
+data BahEither b a
+  = PLeft a
+  | PRight b
+  deriving (Eq, Show)
+
+instance Functor (BahEither b) where
+  fmap _ (PRight b) = PRight b
+  fmap f (PLeft a)  = PLeft $ f a
+
+instance Applicative (BahEither b) where
+  pure = PLeft
+
+  PRight a <*> _ = PRight a
+  _ <*> PRight a = PRight a
+  PLeft f <*> PLeft a = PLeft $ f a
+
+instance Monad (BahEither b) where
+  NopeDotJpg >>= _ = NopeDotJpg
+
+instance Arbitrary a => Arbitrary (Nope a) where
+  arbitrary = return NopeDotJpg
+
+instance EqProp (Nope a) where
+  (=-=) = eq
+
 main :: IO ()
 main = do
   quickBatch (monad $ (Second ("hi", 1, [1, 2] ) :: Sum String (String, Int, [Int])))
+  quickBatch (monad $ (NopeDotJpg :: Nope (String, Int, [Int])))

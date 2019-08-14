b9a8f43c2d02ebb8216592e56d6abc465edad9f3
jeffhappily <goodboy96@live.com.my>
2019-08-14 16:37:05 +0800

    Generate TL.Text responses

--- shawty/app/Main.hs
+++ shawty/app/Main.hs
@@ -34,6 +34,38 @@ saveURI conn shortURI uri = R.runRedis conn $ R.set shortURI uri
 getURI :: R.Connection -> BC.ByteString -> IO (Either R.Reply (Maybe BC.ByteString))
 getURI conn shortURI = R.runRedis conn $ R.get shortURI
 
+linkShorty :: String -> String
+linkShorty shorty =
+  concat
+  [ "<a href=\""
+  , shorty
+  , "\">Copy and paste your short URL</a>"
+  ]
+
+shortyCreated :: Show a => a -> String -> TL.Text
+shortyCreated resp shawty =
+  TL.concat
+  [ TL.pack (show resp)
+  , " shorty is: "
+  , TL.pack (linkShorty shawty)
+  ]
+
+shortyAintUri :: TL.Text -> TL.Text
+shortyAintUri uri =
+  TL.concat
+  [ uri
+  , " wasn't a url,"
+  , " did you forget http://?"
+  ]
+
+shortyFound :: TL.Text -> TL.Text
+shortyFound tbs =
+  TL.concat
+  [ "<a href=\""
+  , tbs, "\">"
+  , tbs, "</a>"
+  ]
+
 main :: IO ()
 main = do
   putStrLn "hello world"

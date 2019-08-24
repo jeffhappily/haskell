5fa72a496e79730b2732bb22b9bbd095bff804df
jeffhappily <goodboy96@live.com.my>
2019-08-24 12:18:23 +0800

    Add chap22

---
+++ chap22.hs
@@ -0,0 +1,24 @@
+module Chap22 where
+
+import           Control.Applicative
+
+boop = (*2)
+doop = (+10)
+
+bip :: Integer -> Integer
+bip = boop . doop
+
+bloop :: Integer -> Integer
+bloop = fmap boop doop
+
+bbop :: Integer -> Integer
+bbop = (+) <$> boop <*> doop
+
+duwop :: Integer -> Integer
+duwop = liftA2 (+) boop doop
+
+boopDoop :: Integer -> Integer
+boopDoop = do
+  a <- boop
+  b <- doop
+  return (a + b)

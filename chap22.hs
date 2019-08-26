e54951842d8799991b78e3219ee8da236a49b3fb
jeffhappily <goodboy96@live.com.my>
2019-08-26 15:51:43 +0800

    Add more exercise

--- chap22.hs
+++ chap22.hs
@@ -1,6 +1,7 @@
 module Chap22 where
 
 import           Control.Applicative
+import           Data.Char
 
 boop = (*2)
 doop = (+10)
@@ -22,3 +23,30 @@ boopDoop = do
   a <- boop
   b <- doop
   return (a + b)
+
+-----------------------
+
+cap :: [Char] -> [Char]
+cap xs = map toUpper xs
+
+rev :: [Char] -> [Char]
+rev xs = reverse xs
+
+composed :: [Char] -> [Char]
+composed = cap . rev
+
+fmapped :: [Char] -> [Char]
+fmapped = cap <$> rev
+
+tupled :: [Char] -> ([Char], [Char])
+tupled = do
+  a <- cap
+  b <- rev
+
+  return (a, b)
+
+tupled' :: [Char] -> ([Char], [Char])
+tupled' = do
+  rev >>= \a ->
+    cap >>= \b ->
+      return (a, b)

41145b7b3c00583b09bfef67a978218117f549c0
jeffhappily <goodboy96@live.com.my>
2019-08-28 11:57:00 +0800

    Finish functor and applicative instance for reader

--- chap22.hs
+++ chap22.hs
@@ -66,10 +66,13 @@ myLiftA2 f a b = f <$> a <*> b
 asks :: (r -> a) -> Reader r a
 asks = Reader
 
+instance Functor (Reader r) where
+  fmap f (Reader r) = Reader $ (f . r)
+
 instance Applicative (Reader r) where
   pure :: a -> Reader r a
   pure a = Reader $ const a
 
   (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
   (Reader rab) <*> (Reader ra) =
-    Reader $ \r -> fmap (rab r) ra
+    Reader $ \r -> rab r (ra r)

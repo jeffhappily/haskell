2e16bd22e0d1b0de7e94c173f07c4aed643b513f
jeffhappily <goodboy96@live.com.my>
2019-09-02 20:28:43 +0800

    Add functor and monad instance for reader

--- chap22.hs
+++ chap22.hs
@@ -66,10 +66,21 @@ myLiftA2 f a b = f <$> a <*> b
 asks :: (r -> a) -> Reader r a
 asks = Reader
 
+instance Functor (Reader r) where
+  fmap f (Reader r) = Reader $ f . r
+
 instance Applicative (Reader r) where
   pure :: a -> Reader r a
   pure a = Reader $ const a
 
   (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
   (Reader rab) <*> (Reader ra) =
-    Reader $ \r -> fmap (rab r) ra
+    Reader $ \r -> rab r (ra r)
+
+instance Monad (Reader r) where
+  return = pure
+
+  (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
+  (Reader ra) >>= aRb =
+    Reader $ \r -> let Reader f = aRb (ra r) in f r
+

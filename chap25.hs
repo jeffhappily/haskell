38eb5ed1baf47bfefadb4d4dfc2eacccbdeddd20
jeffhappily <goodboy96@live.com.my>
2019-09-19 09:43:43 +0800

    Foldable and traversable instance for Compose

--- chap25.hs
+++ chap25.hs
@@ -28,3 +28,26 @@ instance (Applicative f, Applicative g) => Applicative (Compose f g) where
 --     -> (a -> Compose f g b)
 --     -> Compose f g b
 --   (Compose a) >>= f = f a
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+  foldMap f (Compose xs) = foldMap (foldMap f) xs
+
+instance (Traversable f, Traversable g) => Traversable (Compose f g) where
+  traverse f (Compose xs) = Compose <$> traverse (traverse f) xs
+
+---------------------
+
+class Bifunctor p where
+  {-# MINIMAL bimap | first, second #-}
+
+  bimap :: (a -> b)
+    -> (c -> d)
+    -> p a c
+    -> p b d
+  bimap f g = first f . second g
+
+  first :: (a -> b) -> p a c -> p b c
+  first f = bimap f id
+
+  second :: (b -> c) -> p a b -> p a c
+  second = bimap id

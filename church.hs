77a9222c4427995a3d0906f625d4edcd7212f388
Jeff Happily <goodboy96@live.com.my>
2020-05-11 23:14:13 +0800

    More cleaning up

--- church.hs
+++ church.hs
@@ -4,5 +4,5 @@ zero f x = x
 one :: (a -> a) -> a -> a
 one f x = f x
 
-successor :: (a -> a) -> a -> a -> a -> a
+successor :: ((a -> a) -> a -> a) -> (a -> a) -> a -> a
 successor n f x = f $ n f x
--- print2.hs
+++
@@ -1,8 +0,0 @@
-module Print2 where
-
-main :: IO ()
-main = do
-  putStrLn "Count to four for me:"
-  putStr   "one, two"
-  putStr   ", three, and"
-  putStrLn " four!"
